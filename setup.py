8f17a9b9157969d79a3ad6cf0c5c0a88e22124dc
Andrew Pitman <54857711+NerdicanPsycho@users.noreply.github.com>
2021-07-24 02:13:21 -0400

    Added setup script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='Polynomials',
+    version='0.1',
+    packages=['Polynomials'],
+    url='https://github.com/DrewPitman/Polynomials',
+    author='Andrew Pitman',
+    author_email='drewgpitman@gmail.com',
+    description='Polynomials defined in terms of indeterminates'
+)
